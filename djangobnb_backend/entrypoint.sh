90f46bef2f7b71743aee12c3b4d9e842c80ce383
Leonardo Jeff Cadiz <leonardojeff.cadiz@msunaawan.edu.ph>
2025-11-22 16:15:36 +0800

    Cadiz - Video 3 set up docker

---
+++ djangobnb_backend/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$DATABASE" = "postgres" ]; then
+    echo "Checking if database is running..."
+
+    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
+        sleep 0.1
+    done
+
+    echo "The database is up and running :-D"
+fi
+
+python manage.py migrate --noinput
+
+exec "$@"
