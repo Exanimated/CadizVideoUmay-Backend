88b44454ffe9f87452845b53f954a03987e6b306
Sherry Jean Codaste <sherryjean.codaste@msunaawaan.edu.ph>
2025-12-01 13:49:34 +0800

    Cadiz video 5 - add properties from nexjs (create_modal,api_endpoint,send-date)-backend

--- djangobnb_backend/property/serializers.py
+++ djangobnb_backend/property/serializers.py
@@ -1,13 +1,51 @@
 from rest_framework import serializers
 
-from .models import Property
+from .models import Property, Reservation
+
+from useraccount.serializers import UserDetailSerializer
+
+
 
 class PropertiesListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
         fields = (
             'id',
+            'tittle',    
+            'price_per_night',
+            'image_url'
+            
+        )
+
+class PropertiesDetailsSerializer(serializers.ModelSerializer):
+    landlord = UserDetailSerializer(read_only=True, many=False)
+
+    class Meta:
+        model = Property
+        fields = (
+            'id',
             'tittle',
+            'description',
             'price_per_night',
             'image_url',
+            'bedrooms',
+            'bathrooms',
+            'guests',
+            'landlord'
+           
+            
+        )
+
+class ReservationsListSerializer(serializers.ModelSerializer):
+    property = PropertiesListSerializer(read_only=True, many=False)
+
+    class Meta:
+        model = Reservation
+        fields = (
+            'id',
+            'start_date',
+            'property',
+            'end_date',
+            'number_of_nights',
+            'total_price',
         )
