c9d0f948be146a47bd1ee4398d060583c2bb63d3
Exan <leonardojeff.cadiz@msunaawan.edu.ph>
2025-12-15 13:07:26 +0800

    Cadiz video 7 - make favorite page (wishlisting)

--- djangobnb_backend/property/admin.py
+++ djangobnb_backend/property/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 
-from .models import Property
+from .models import Property, Reservation
 
-admin.site.register(Property)
+
+class ReservationInline(admin.TabularInline):
+	model = Reservation
+	extra = 0
+	readonly_fields = (
+		"start_date",
+		"end_date",
+		"number_of_nights",
+		"guests",
+		"total_price",
+		"created_by",
+		"created_at",
+	)
+	can_delete = True
+
+
+class PropertyAdmin(admin.ModelAdmin):
+	list_display = ("tittle", "landlord", "price_per_night", "created_at")
+	inlines = [ReservationInline]
+
+
+admin.site.register(Property, PropertyAdmin)
+admin.site.register(Reservation)
